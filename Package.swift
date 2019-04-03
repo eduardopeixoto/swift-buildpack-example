cf55c1fc7ac48c225d53c46fc5b0dedb075b4c73
Luiz Eduardo <luiz.eduardo.peixoto@gmail.com>
2019-04-03 15:58:55 -0300

    Corrected package version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "HelloWorld",
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMajor(from: "2.6.2")),
+    .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 2),
   ]
 )
